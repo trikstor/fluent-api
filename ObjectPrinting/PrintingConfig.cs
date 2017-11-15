d15d25c395ed39d5742e0c4a45e298e86ef143b4
promo\Acer <azpk360@yandex.ru>
2017-11-15 21:47:28 +0500

    Наработки с занятия.

--- ObjectPrinting/PrintingConfig.cs
+++ ObjectPrinting/PrintingConfig.cs
@@ -1,14 +1,43 @@
 using System;
+using System.Globalization;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Net;
+using System.Reflection;
 using System.Text;
 
 namespace ObjectPrinting
 {
+    public interface IPropertyPrintingConfig<TOwner, TPropType>
+    {
+        PrintingConfig<TOwner> PrintingConfig {get;}
+    }
+
     public class PrintingConfig<TOwner>
     {
-        public string PrintToString(TOwner obj)
+        public PrintingConfig<TOwner> ExcludeType<TPropType>()
+        {
+            return this;
+        }
+
+    public string PrintToString(TOwner obj)
         {
             return PrintToString(obj, 0);
+        }
+
+        public PrintingConfig<TOwner> GetOtherSerialize<T>(T type, Func<T, string> func)
+        {
+            return this;
+        }
+
+        public PropertyPrintingConfig<TOwner, TPropType> Printing<TPropType>()
+        {
+            return new PropertyPrintingConfig<TOwner, TPropType>(this);
+        }
+
+        public PropertyPrintingConfig<TOwner, TPropType> Printing<TPropType>(Expression<Func<TOwner, TPropType>> selector)
+        {
+            return new PropertyPrintingConfig<TOwner, TPropType>(this);
         }
 
         private string PrintToString(object obj, int nestingLevel)
@@ -36,6 +65,31 @@ namespace ObjectPrinting
                               nestingLevel + 1));
             }
             return sb.ToString();
+        }
+    }
+
+    public class PropertyPrintingConfig<TOwner, TPropType> : IPropertyPrintingConfig<TOwner, TPropType>
+    {
+        private readonly PrintingConfig<TOwner> printingConfig;
+        public PropertyPrintingConfig(PrintingConfig<TOwner> printingConfig)
+        {
+            this.printingConfig = printingConfig;
+        }
+
+        public PrintingConfig<TOwner> Using(Func<TPropType, string> serializeFunc)
+        {
+            return printingConfig;
+        }
+
+        PrintingConfig<TOwner> IPropertyPrintingConfig<TOwner, TPropType>
+            .PrintingConfig => printingConfig;
+    }
+
+    public static class PropertyPrintingConfigExtention
+    {
+        public static PrintingConfig<TOwner> Using<TOwner>(this PropertyPrintingConfig<TOwner, int> propertyPrintingConfig, CultureInfo cultureInfo)
+        {
+            return ((IPropertyPrintingConfig<TOwner, int>) propertyPrintingConfig).PrintingConfig;
         }
     }
 }
